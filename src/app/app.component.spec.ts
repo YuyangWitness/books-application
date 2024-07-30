e17c9e235248439c6129d2ce91fe9e1ba88bb2f5
Yu, Yang Claire <jxndyy2015@163.com>
2024-07-30 14:53:44 +0800

    project init

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { AuthService } from './core/auth/services/auth.service';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+      providers: [AuthService, HttpClient, HttpHandler]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
